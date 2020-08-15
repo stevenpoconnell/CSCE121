7e74a649e6978d58b3a0e35ad01c6a5a29576ccf
stevenpoconnell <69654841+stevenpoconnell@users.noreply.github.com>
2020-08-15 14:00:35 -0500

    Create functions.h

---
+++ Assignment 2/Different Digits/functions.h
@@ -0,0 +1,15 @@
+#ifndef functions_h
+#define functions_h
+
+bool is_valid_range(int a, int b);
+
+int count_digit_occurrences(int number, int digit);
+
+bool has_repeated_digits(int number);
+
+int count_valid_numbers(int a, int b);
+
+#endif
+
+
+
