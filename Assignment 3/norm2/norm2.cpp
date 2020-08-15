cda8f038b3463a0720ff50ada2a375a75d7e90fc
stevenpoconnell <69654841+stevenpoconnell@users.noreply.github.com>
2020-08-15 14:04:25 -0500

    Add files via upload

---
+++ Assignment 3/norm2/norm2.cpp
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "\nEnter a number: ";
+    double x = 0;
+    cin >> x;
+    
+    x = 1.5 * x;
+    
+    cout << "\n3 times x is: " << x << endl;
+    
+}
